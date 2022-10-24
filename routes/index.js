c0890cde191e78bf6ae854ede90850761c7f6deb
Bret Borden <bordenb21@gmail.com>
2022-10-24 09:58:19 -0400

    added routers to complete index js in routes directory

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,16 @@ const { Router } = require('express')
 const controllers = require('../controllers')
 const router = Router()
 
+router.get('/', (req, res) => res.send('This is root!'))
+
+router.post('/collections', controllers.createCollection)
+
+router.post('/collections', controllers.getAllCollections)
+
+router.get('/collections/:id', controllers.getCollectionById)
+
+router.put('/collections/:id', controllers.updateCollection)
+
+router.delete('/collections/:id', controllers.deleteCollection)
+
 module.exports = router
