54788115d9ef9663501a0c30a128900a8d653aa6
Bret Borden <bordenb21@gmail.com>
2022-10-30 09:15:55 -0400

    removed unused variables

--- client/src/pages/PhraseByTheme.js
+++ client/src/pages/PhraseByTheme.js
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 const PhraseByTheme = () => {
-  let navigate = useNavigate()
-
   const [phraseTheme, updateCollections] = useState([])
   const [addPhrase, setAddPhrase] = useState({
     theme: '',
--- client/src/pages/VocabLists.js
+++ client/src/pages/VocabLists.js
@@ -1,10 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
 
 const VocabLists = () => {
-  let navigate = useNavigate()
-
   const [phrases, updatePhrases] = useState([])
   const [changePhrase, setPhrase] = useState({
     checkPhrase: ''
