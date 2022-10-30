e2c63bf0202658032e4883b378590c7934369a7a
Bret Borden <bordenb21@gmail.com>
2022-10-30 09:26:39 -0400

    added delete method to the VocabList page that displays all phrases

--- client/src/pages/VocabLists.js
+++ client/src/pages/VocabLists.js
@@ -8,6 +8,9 @@ const VocabLists = () => {
   })
   const [input, setInput] = useState('')
 
+  //add delete method here
+  const [remove, removePhrase] = useState('')
+
   const apiCall = async () => {
     let response = await axios.get('http://localhost:3001/phrases')
     updatePhrases(response.data)
@@ -39,6 +42,19 @@ const VocabLists = () => {
 
   const inputChange = (e) => {
     setInput({ ...input, [e.target.id]: e.target.value })
+  }
+
+  //add delete function
+  const deletePhrase = async (e) => {
+    let response = await axios.delete(
+      `http://localhost:3001/phrases/${remove.phrase}`
+    )
+    apiCall()
+  }
+
+  //add handle for delete
+  const handlePhraseChange = async (e) => {
+    removePhrase({ ...remove, [e.target.id]: e.target.value })
   }
 
   return (
@@ -74,6 +90,24 @@ const VocabLists = () => {
           Update Phrase
         </button>
       </form>
+      {/* added remove button */}
+      <br />
+      <label className="formLabel" htmlFor="phrase">
+        Select Phrase:{' '}
+      </label>
+      <select
+        className="deleteDropdown"
+        id="phrase"
+        onChange={handlePhraseChange}
+      >
+        <option></option>
+        {phrases.map((phrase) => (
+          <option value={phrase._id}>{phrase.phrase}</option>
+        ))}
+      </select>
+      <button className="homeBtn" type="submit" onClick={deletePhrase}>
+        Remove Phrase
+      </button>
     </div>
   )
 }
